3aeaa647ce5913aaddd573d4240d98d919b0b9a9
andreeamacsim <mlaviniaandreea@yahoo.com>
2023-10-24 16:48:35 +0300

    I added new members to the "Game" class, then I built the constructors of the class, as well as the specific settings

--- ProjectC++/ProjectC++/Game.cpp
+++ ProjectC++/ProjectC++/Game.cpp
@@ -1,1 +1,38 @@
 #include "Game.h"
+Game::Game()
+{
+}
+
+Game::Game(const std::vector<Player>& players, const std::vector<Round>& rounds, const std::vector<Word>& wordList,  uint8_t currentRound, Player currentDrawer)
+	: m_players(players)
+	, m_rounds(rounds)
+	, m_wordList(wordList)
+	, m_currentRound(currentRound)
+	, m_currentDrawer(currentDrawer)
+{
+}
+
+void Game::setPlayers( const std::vector<Player>& players)
+{
+	m_players = players;
+}
+
+void Game::setRounds(const std::vector<Round>& rounds)
+{
+	m_rounds = rounds;
+}
+
+void Game::setWordList(const std::vector<Word>& wordList)
+{
+	m_wordList = wordList;
+}
+
+void Game::setCurrentRound(uint8_t currentRound)
+{
+	m_currentRound = currentRound;
+}
+
+void Game::setCurrentPlayer(Player currentDrawer)
+{
+	m_currentDrawer = currentDrawer;
+}
--- ProjectC++/ProjectC++/Game.h
+++ ProjectC++/ProjectC++/Game.h
@@ -2,16 +2,27 @@
 #include <vector>
 #include "Player.h"
 #include "Round.h"
+#include "Word.h"
 class Game
 {
 public:
+	Game();
+	Game(const std::vector<Player>& players, const std::vector<Round>& rounds, const std::vector<Word>& wordList,  uint8_t currentRound,  Player currentDrawer);
+
+	void setPlayers( const std::vector<Player>& players);
+	void setRounds(const std::vector<Round>& rounds);
+	void setWordList(const std::vector<Word>& wordList);
+	void setCurrentRound(uint8_t currentRound);
+	void setCurrentPlayer(Player currentDrawer);
+
+
 
 private:
 	std::vector<Player> m_players;
 	std::vector<Round> m_rounds;
+	std::vector<Word> m_wordList;
 	uint8_t m_currentRound;
-
-
+	Player m_currentDrawer;
 
 };
 
