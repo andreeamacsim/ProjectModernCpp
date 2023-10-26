c631a808d00f2a0eb3e86aaccc197156394756b1
MihaiMoisescu <mihaimoisescu9@gmail.com>
2023-10-26 18:01:02 +0300

    I implemented the specific methods in the "Player.cpp" file

--- ProjectC++/ProjectC++/Player.cpp
+++ ProjectC++/ProjectC++/Player.cpp
@@ -1,1 +1,34 @@
 #include "Player.h"
+
+Player::Player()
+{
+}
+
+Player::Player(std::string username, std::string password, Profile playerProfile):m_username(username),m_password(password),m_playerProfile(playerProfile)
+{
+}
+
+std::string Player::getUsername()
+{
+	return this->m_username;
+}
+
+std::string Player::getPassword()
+{
+	return this->m_username;
+}
+
+Profile Player::getProfile()
+{
+	return this->m_playerProfile;
+}
+
+void Player::setUsername(std::string username)
+{
+	this->m_username = username;
+}
+
+void Player::setPassword(std::string password)
+{
+	this->m_password=password
+}
