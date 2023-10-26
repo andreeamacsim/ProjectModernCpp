8fd12b6ff6e97dc17d6fb8a4e9eef3dae1e6c37e
MihaiMoisescu <mihaimoisescu9@gmail.com>
2023-10-26 17:48:29 +0300

    I created the signatures of the constructors, getters and setters in the "Player" header

--- ProjectC++/ProjectC++/Player.h
+++ ProjectC++/ProjectC++/Player.h
@@ -1,5 +1,24 @@
 #pragma once
+#include <string>
+#include "Profile.h"
 class Player
 {
+public:
+	Player();
+	Player(std::string username, std::string password,Profile playerProfile);
+
+	std::string getUsername();
+	std::string getPassword();
+	Profile getProfile();
+
+
+	void setUsername(std::string username);
+	void setPassword(std::string password);
+
+private:
+	std::string m_username;
+	std::string m_password;
+	Profile m_playerProfile;
+
 };
 
