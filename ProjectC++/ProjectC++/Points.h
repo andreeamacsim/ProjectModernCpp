e463f5e3ae28f9f4f2b948112c36daa46e53b0bb
CristinaIuliaParaschiv <iulia.cristina_paraschiv@yahoo.com>
2023-10-28 16:29:01 +0300

    I created the class named Points and I defined some methods.

---
+++ ProjectC++/ProjectC++/Points.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+class Points
+{
+public:
+	uint32_t GetPoints();
+	uint32_t SetPoints();
+	Points();
+	uint32_t PointsPerRound();//punctele adaugate fiecarui jocator per runda jucata
+private:
+	uint32_t points;
+
+};
+
