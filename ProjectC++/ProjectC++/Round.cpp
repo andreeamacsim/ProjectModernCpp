d0af8a24b12742546114e95d770931c809913129
andreeamacsim <mlaviniaandreea@yahoo.com>
2023-10-30 17:10:26 +0200

    I wrote the members and methods currently needed for the Round header.

--- ProjectC++/ProjectC++/Round.cpp
+++ ProjectC++/ProjectC++/Round.cpp
@@ -1,1 +1,38 @@
 #include "Round.h"
+
+Round::Round()
+{
+}
+
+Round::Round(const std::string& word, const std::string& drawer)
+{
+}
+
+void Round::AddGuess(const std::string& guess)
+{
+}
+
+bool Round::IsGuessed() const
+{
+	return false;
+}
+
+const std::string& Round::GetWord()
+{
+	// TODO: insert return statement here
+}
+
+const std::string& Round::GetDrawer()
+{
+	// TODO: insert return statement here
+}
+
+const std::vector<std::string>& Round::GetGuesses()
+{
+	// TODO: insert return statement here
+}
+
+std::vector<std::string> Round::GetGuessesForPlayer(const std::string& player)
+{
+	return std::vector<std::string>();
+}
--- ProjectC++/ProjectC++/Round.h
+++ ProjectC++/ProjectC++/Round.h
@@ -1,5 +1,22 @@
 #pragma once
+import <vector>;
+import <string>;
 class Round
 {
+public:
+    Round();
+    Round(const std::string& word, const std::string& drawer);
+    void AddGuess(const std::string& guess);
+    bool IsGuessed() const;
+    const std::string& GetWord();
+    const std::string& GetDrawer() ;
+    const std::vector<std::string>& GetGuesses() ;
+    std::vector<std::string> GetGuessesForPlayer(const std::string& player) ;
+
+private:
+	std::string m_word;
+	std::string m_drawer;
+	std::vector<std::string> m_guesses;
+	bool m_guessed;
 };
 
