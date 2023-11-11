c70f051ceedc46cffeb4b47de416f66d7c7eab94
CristinaIuliaParaschiv <iulia.cristina_paraschiv@yahoo.com>
2023-11-11 16:26:31 +0200

    I implemented the module Word.

--- ProjectC++/ProjectC++/Word.cpp
+++ ProjectC++/ProjectC++/Word.cpp
@@ -1,1 +1,17 @@
+import <string>;
+import <vector>;
+import <fstream>;
+import <cstdlib>;
+import <ctime>;
+import <iostream>;
 module word;
+
+std::string selectRandomWord(const std::vector<std::string>& words)
+{
+    if (verify())
+    {
+        int randomIndex = rand() % words.size();
+        return words[randomIndex];
+        
+    }
+}
