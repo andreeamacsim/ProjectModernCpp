0a06abfde38bc091df0b576bb6f3d792da7d5009
CristinaIuliaParaschiv <iulia.cristina_paraschiv@yahoo.com>
2023-11-11 17:11:44 +0200

    I added a new method for the module Word.

--- ProjectC++/ProjectC++/Word.cpp
+++ ProjectC++/ProjectC++/Word.cpp
@@ -6,12 +6,41 @@ import <ctime>;
 import <iostream>;
 module word;
 
-std::string selectRandomWord(const std::vector<std::string>& words)
+std::string selectRandomWord(const std::vector<std::string>& m_words)
 {
     if (verify())
     {
-        int randomIndex = rand() % words.size();
-        return words[randomIndex];
-        
+        int randomIndex = rand() % m_words.size();
+        return m_words[randomIndex];
     }
+}
+
+void generateWords(std::vector<std::string>m_words)
+{
+    std::ifstream inputFile("words.txt");
+    std::string word;
+    while (std::getline(inputFile, word)) {
+        m_words.push_back(word);
+    }
+}
+
+std::string revealCharacter(const std::string& word)
+{
+    srand(static_cast<unsigned>(time(nullptr)));
+    uint16_t maximumLetters = word.length() / 2;
+    uint16_t letterCount = 0;
+    for (int i = 0; i < word.length(); i++ && letterCount < maximumLetters)
+    {
+        //TBA
+    }
+}
+
+bool verify()
+{
+    std::ifstream inputFile("words.txt");
+    if (!inputFile.is_open()) {
+        std::cerr << "Eroare la deschiderea fi?ierului cu cuvinte.\n";
+        return false;
+    }
+    return true;
 }
