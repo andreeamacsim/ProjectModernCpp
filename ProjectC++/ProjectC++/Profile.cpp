88a7853009fc350e81a9a9dc1bbf4452435acd44
CristinaIuliaParaschiv <iulia.cristina_paraschiv@yahoo.com>
2023-11-11 15:24:08 +0200

    I implemented the methods previously created for the module and class Profile.

--- ProjectC++/ProjectC++/Profile.cpp
+++ ProjectC++/ProjectC++/Profile.cpp
@@ -1,25 +1,50 @@
 module profile;
 std::string Profile::getPlayerName() const
 {
-    return std::string();
+    return this->m_playerName;
 }
 
 uint16_t Profile::getAge() const
 {
-    return 0;
+    return this-> m_age;
 }
 
 std::string Profile::getCountry() const
 {
-    return std::string();
+    return this->m_country;
 }
 
 uint32_t Profile::getLevel() const
 {
-    return 0;
+    return this->m_level;
 }
 
 std::string Profile::getDescription() const
 {
-    return std::string();
+    return this->m_description;
+}
+
+void Profile::setPlayerName(const std::string& playerName)
+{
+    this->m_playerName = playerName;
+}
+
+void Profile::setAge(int age)
+{
+    this->m_age = age;
+}
+
+void Profile::setCountry(const std::string& country)
+{
+    this->m_country = country;
+}
+
+void Profile::setLevel(int level)
+{
+    this->m_level = level;
+}
+
+void Profile::setDescription(const std::string& description)
+{
+    this->m_description = description;
 }
