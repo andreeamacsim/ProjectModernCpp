7251ddde98a6a09290fd334a6c10b4601c45fe65
CristinaIuliaParaschiv <iulia.cristina_paraschiv@yahoo.com>
2023-11-17 14:13:25 +0200

    Update to Word.cpp

--- ProjectC++/ProjectC++/Word.cpp
+++ ProjectC++/ProjectC++/Word.cpp
@@ -1,4 +1,5 @@
-﻿import <string>;
+﻿module word;
+import <string>;
 import <vector>;
 import <fstream>;
 import <cstdlib>;
@@ -16,32 +17,22 @@ bool verify()
     return true;
 }
 
-std::string selectRandomWord(const std::vector<std::string>& m_words)
+void Word::generateWords()
+{
+    std::ifstream inputFile("words.txt");
+    std::string word;
+    while (std::getline(inputFile, word)) 
+    {
+        m_words.push_back(word);
+    }
+}
+
+std::string Word::selectRandomWord(const std::vector<std::string>& m_words)
 {
     if (verify())
     {
         int randomIndex = rand() % m_words.size();
         return m_words[randomIndex];
-    }
-}
-
-void generateWords(std::vector<std::string>m_words)
-{
-    std::ifstream inputFile("words.txt");
-    std::string word;
-    while (std::getline(inputFile, word)) {
-        m_words.push_back(word);
-    }
-}
-
-std::string revealCharacter(const std::string& word)
-{
-    srand(static_cast<unsigned>(time(nullptr)));
-    uint16_t maximumLetters = word.length() / 2;
-    uint16_t letterCount = 0;
-    for (int i = 0; i < word.length(); i++ && letterCount < maximumLetters)
-    {
-        //TBA
     }
 }
 
