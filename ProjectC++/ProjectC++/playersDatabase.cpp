c1628afda2f873654041310fd124dd69e0244b17
MihaiMoisescu <mihaimoisescu9@gmail.com>
2023-12-03 10:18:51 +0200

    I added and implemented the function for adding players to the database

--- ProjectC++/ProjectC++/playersDatabase.cpp
+++ ProjectC++/ProjectC++/playersDatabase.cpp
@@ -1,1 +1,16 @@
 #include "playersDatabase.h"
+
+std::vector<Player> PlayerStorage::getPlayers()
+{
+	return m_db.get_all<Player>();
+}
+
+void PlayerStorage::AddPlayerToStorage(std::string username, std::string password, std::string email)
+{
+	Player p;
+	p.setUsername(username);
+	p.setEmail(email);
+	p.setPassword(password);
+	m_db.insert(p);
+	//m_db.insert(Player{username,password,email})
+}
--- ProjectC++/ProjectC++/playersDatabase.h
+++ ProjectC++/ProjectC++/playersDatabase.h
@@ -7,31 +7,36 @@
 #include <sqlite_orm/sqlite_orm.h>
 
 namespace sql = sqlite_orm;
-
+import profile;
 import player;
 
-inline auto CreateStorage(const std::string& filename)
-{
-	return sql::make_storage(
-		filename,
-		sql::make_table(
-			"Player",
-			sql::make_column("username", &Player::setUsername, &Player::getUsername, sql::primary_key()),
-			sql::make_column("password", &Player::setPassword, &Player::getPassword),
-			sql::make_column("e-mail", &Player::setEmail, &Player::getEmail)
-		)
-	);
+inline auto CreateStorage(const std::string& filename) {
+    return sql::make_storage(
+        filename,
+        sql::make_table(
+            "Player",
+            sql::make_column("username", &Player::setUsername, &Player::getUsername, sql::primary_key()),
+            sql::make_column("password", &Player::setPassword, &Player::getPassword),
+            sql::make_column("email", &Player::setEmail, &Player::getEmail)
+        )
+        //sql::make_table(
+//	"Profile",
+//	sql::make_column("username", &Profile::getPlayerName, &Profile::setPlayerName, sql::primary_key()),
+//	sql::make_column("age",&Profile::getAge,&Profile::setAge),
+//	sql::make_column("country",&Profile::getCountry,&Profile::setCountry),
+//	sql::make_column("level",&Profile::getLevel,&Profile::setLevel),
+//	sql::make_column("description",&Profile::getDescription,&Profile::setDescription),
+//)
+    );
 }
+
 using Storage = decltype(CreateStorage(""));
 
-
-class PlayerStorage
-{
+class PlayerStorage {
 public:
+    std::vector<Player>getPlayers();
+    void AddPlayerToStorage(std::string username, std::string password, std::string email);
 private:
-
-private:
-	const std::string kDbFile{ "players.sqlite" };
-private:
-	Storage m_db = CreateStorage(kDbFile);
+    const std::string kDbFile{ "players.sqlite" };
+    Storage m_db = CreateStorage(kDbFile);
 };
